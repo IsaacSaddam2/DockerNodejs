ca2ac8eec2d915ee81e7215f24d84d1a4b5ad29c
Isaac Saddam <saddamk0410@gmail.com>
2021-11-05 12:55:25 +0530

    Connected using mongoose

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose
+// .connect("mongodb://saddam:saddam98@172.22.0.2:27017/?authSource=admin")  //connection string 
+.connect("mongodb://saddam:saddam98@mongo:27017/?authSource=admin")  //connection string  mongo is a servicename in docker-compose file 
+.then(()=>{
+    console.log("connected");
+})
+.catch((e)=>{
+    console.log(e);
+});
 
 app.get('/',(req,res)=>{
     res.send('Hi there u yes u,sorry!');
