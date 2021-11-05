ee02b360837b3f10ec4705a13f1b4d49733f7db1
Isaac Saddam <saddamk0410@gmail.com>
2021-11-05 15:44:46 +0530

    Organized env variable

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MONGO_PASSWORD, MONGO_IP, MONGO_PORT, MONGO_USER } = require('./config/config');
 
 const app = express();
 
+//"mongodb://saddam:saddam98@172.22.0.2:27017/?authSource=admin"  //connection string
+
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+console.log(mongoUrl);
+
 mongoose
-// .connect("mongodb://saddam:saddam98@172.22.0.2:27017/?authSource=admin")  //connection string 
-.connect("mongodb://saddam:saddam98@mongo:27017/?authSource=admin")  //connection string  mongo is a servicename in docker-compose file 
+.connect(mongoUrl)  //connection string  mongo is a servicename in docker-compose file 
 .then(()=>{
     console.log("connected");
 })
